0fd0c1ceb7503047ce8e04b61c1b83410a24e633
James <75079031+AnUnderratedPerson@users.noreply.github.com>
2022-07-08 00:42:43 +0000

    Something i think

--- include/Index.cpp
+++
@@ -1,6 +0,0 @@
-#include "./Index.hpp"
-
-template <typename T>
-void Test::PrintValue(T V) {
-    cout << V << endl;
-}
--- include/Index.hpp
+++ include/Index.hpp
@@ -1,14 +1,10 @@
 #include <iostream>
 #include <string>
 
-#ifndef STD
-
-#define STD
+#ifndef NAMESPACE_STD
+#define NAMESPACE_STD
 using namespace std;
-
 #endif
 
-namespace Test {
-    template <typename T>
-    void PrintValue(T);
-}
+// A trashy garbage collector created for an unknown use...
+namespace GarbageCollector {}
